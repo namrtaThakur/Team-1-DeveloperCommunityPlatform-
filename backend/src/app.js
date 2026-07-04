d1620c809b133610c083bdc632927992e93bead5
Rudra Pratap Singh <rps031ab@gmail.com>
2026-07-04 21:43:38 +0530

    feat: enable CORS for frontend integration

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 const postRoutes = require("./routes/postRoutes");
 const loggerMiddleware = require("./middlewares/loggerMiddleware");
 const timeMiddleware = require("./middlewares/timeMiddleware");
+
+app.use(cors());         
 app.use(loggerMiddleware);
 app.use(timeMiddleware);
 app.use(express.json());
